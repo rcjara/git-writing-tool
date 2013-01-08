a693129a58cc3f2c849746a7d224a21aca16b078
Raul Jara <raul.c.jara@gmail.com>
2013-01-08 15:41:04 -0500

    Refactor SectionDisplayer to use a private Text class

--- lib/section_displayer.rb
+++ lib/section_displayer.rb
@@ -12,25 +12,57 @@ module GWT
     end
 
     def cleaned_text
-      cr section.text.gsub(/(?<=\S)(\n)(?=\S)/,'  ')
+      text.lines_joined
+          .cr
+          .to_s
     end
 
     def cleaned_heading
-      return '' if section.heading.nil?
-      "\n" + cr(section.heading) + "\n"
-    end
-
-    # 'cr' stands for carriage return
-    # ensures that a string ends with (at least)
-    # one new line character.
-    def cr(text)
-      text.chomp + "\n"
+      return '' if heading.empty?
+      "\n" + heading.cr.to_s + "\n"
     end
 
     def self.compose(*args, &block)
       composer = self.new(*args)
       composer.instance_eval &block
       composer.formatted_text
+    end
+
+    private
+
+    def text
+      Text.new(section.text)
+    end
+
+    def heading
+      Text.new(section.heading)
+    end
+
+    class Text
+      attr_reader :text
+
+      def initialize(text)
+        @text = text || ''
+      end
+
+      def empty?
+        text.empty?
+      end
+
+      def to_s
+        text
+      end
+
+      def lines_joined
+        self.class.new text.gsub(/(?<=\S)(\n)(?=\S)/,'  ')
+      end
+
+      # 'cr' stands for carriage return
+      # ensures that a string ends with (at least)
+      # one new line character.
+      def cr
+        self.class.new text.chomp + "\n"
+      end
     end
   end
 end
